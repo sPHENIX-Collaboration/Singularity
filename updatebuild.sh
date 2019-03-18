a7f301772206896cc5686c3ee1eba63115b65b3f
Jin Huang <jhuang@bnl.gov>
2019-03-18 14:14:32 -0400

    Add default parameter and util downloads

--- updatebuild.sh
+++ updatebuild.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
+# Default parameter
 build='new';
 # build='root6';
-
 URLBase='https://www.phenix.bnl.gov/WWW/publish/phnxbld/sPHENIX/Singularity/';
 DownloadBase='cvmfs/sphenix.sdcc.bnl.gov/';
 
+# Parse input parameter
+for i in "$@"
+do
+case $i in
+    -b=*|--build=*)
+    build="${i#*=}"
+    shift # past argument=value
+    ;;
+    -s=*|--source=*)
+    URLBase="${i#*=}"
+    shift # past argument=value
+    ;;
+    -t=*|--target=*)
+    DownloadBase="${i#*=}"
+    shift # past argument=value
+    ;;
+    --help|-h|*)
+    echo "Usage: $0 [--build=<new|root6>] [--source=URL] [--target=directory]";
+    exit;
+    shift # past argument with no value
+    ;;
+esac
+done
+
 echo "--------------------------------------------------------"
-echo "This macro download/update sPHENIX builds to $DownloadBase"
+echo "This macro download/update sPHENIX ${build} build to $DownloadBase"
+echo "Source is at $URLBase"
 echo ""
 echo "If you have CVMFS directly mounted on your computer,"
 echo "you can skip this download and mount /cvmfs/sphenix.sdcc.bnl.gov to singularity directly."
@@ -38,13 +63,21 @@ wget --quiet --no-check-certificate --directory-prefix=${DownloadBase}/ ${URLBas
 
 echo "--------------------------------------------------------"
 echo "Downloading and decompress ${DownloadBase}/ ${URLBase}/${build}/offline_main.tar.bz2 "
-echo "This might take a while ...."
+echo "This might take a while too ...."
 echo "--------------------------------------------------------"
 
 # rsync -al --delete --progress rftpexp.rhic.bnl.gov:/cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/release/new/ cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/release/new/;
 # https://www.phenix.bnl.gov/phenix/WWW/publish/phnxbld/sPHENIX/Singularity/new/offline_main.tar.bz2
 
 wget --quiet --no-check-certificate --directory-prefix=${DownloadBase}/ ${URLBase}/${build}/offline_main.tar.bz2 -O- | tar xjf -  
+
+echo "--------------------------------------------------------"
+echo "Downloading and decompress ${DownloadBase}/ ${URLBase}/${build}/utils.tar.bz2 "
+echo "This might take a while too ...."
+echo "--------------------------------------------------------"
+# https://www.phenix.bnl.gov/phenix/WWW/publish/phnxbld/sPHENIX/Singularity/new/utils.tar.bz2
+
+wget --quiet --no-check-certificate --directory-prefix=${DownloadBase}/ ${URLBase}/${build}/utils.tar.bz2 -O- | tar xjf -  
 
 echo "--------------------------------------------------------"
 echo "Done! To run the sPHENIX container in shell mode:"
